2834c66c9013dc025cc9df1d7979398d7f49649f
maaa <marachckow2013@yandex.ru>
2021-07-08 08:42:31 +0300

    Initial commit

---
+++ src/main/java/com/test/project/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.test.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String listAllFunFiction() {
+        return "templates/index";
+    }
+}
