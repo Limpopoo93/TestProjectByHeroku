9a9539df48a74c69326abfabbf9b559fb54d7166
maaa <marachckow2013@yandex.ru>
2021-07-08 09:52:03 +0300

    1

--- src/main/java/com/test/project/SpringBootApplication.java
+++ src/main/java/com/test/project/SpringBootApplication.java
@@ -1,10 +1,16 @@
 package com.test.project;
 
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
+    }
+    @RequestMapping("/index")
+    public String listAllFunFiction() {
+        return "index";
     }
 }
